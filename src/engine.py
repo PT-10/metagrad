95bba642ae651996e617f06fbd2cfbbddcfc24cc
PT-10 <tandonprashant101@gmail.com>
2024-07-09 10:36:13 +0530

    Structuring the flow for addition operation

--- src/engine.py
+++ src/engine.py
@@ -10,16 +10,55 @@ class Tensor:
        self.data = data if isinstance(data, np.ndarray) else np.array(data)
        self._opt = _opt
        self._prev = set(_children)
+    #    self.requires_grad = False
+    #    self.grad = np.zeros_like(self.data) if self.requires_grad else None
+       self.grad = np.zeros_like(self.data)
+       self._backward = lambda: None
 
     def __repr__(self):
-        return f"Tensor(data={self.data}, dtype={type(self.data)})"
-
+        # return f"Tensor(data={self.data}, dtype={type(self.data)})"
+        return f"Tensor(data={self.data}, grad={self.grad}, children={self._prev})"
 
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            # if self.requires_grad:
+                self.grad += out.grad
+            # if other.requires_grad:
+                other.grad += out.grad
+        out._backward = _backward
         return out
     
     def __radd__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
-        return Tensor(other.data + self.data, (other, self), '+')
+        out = Tensor(other.data + self.data, (other, self), '+')
+
+        def _backward():
+            # if self.requires_grad:
+                self.grad += out.grad
+            # if other.requires_grad:
+                other.grad += out.grad
+        out._backward = _backward
+
+        return out
+    
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        # go one variable at a time and apply the chain rule to get its gradient
+        self.grad = np.ones_like(self.data)
+        print(reversed(topo))
+        for v in reversed(topo):
+            v._backward()
+        
--- src/trial.py
+++ src/trial.py
@@ -1,7 +1,15 @@
 from engine import Tensor
+import numpy as np
 
 tensor_a = Tensor([1,2])
 tensor_b = [3,1]
-out = tensor_b + tensor_a
-print("Addition:", out)
-print(out._prev)
+out = tensor_a + tensor_b
+out.backward()
+# print("Addition:", out)
+# out.grad = np.ones_like(out.data)
+# out._backward()
+# print(out.grad)
+# print("Gradients of children")
+# print(tensor_a.grad)
+# print(tensor_b.grad)
+print(out)
