bb51b3811edef12e28016472a0c18ac072df8dcf
PT-10 <tandonprashant101@gmail.com>
2024-07-12 19:19:07 +0530

    Crude forward and backward pass for addition, with/without grad

--- src/engine.py
+++ src/engine.py
@@ -10,23 +10,36 @@ class Tensor:
        self.data = data if isinstance(data, np.ndarray) else np.array(data)
        self._opt = _opt
        self._prev = set(_children)
-    #    self.requires_grad = False
-    #    self.grad = np.zeros_like(self.data) if self.requires_grad else None
-       self.grad = np.zeros_like(self.data)
+       self.requires_grad = False
+       self.grad = np.zeros_like(self.data) if self.requires_grad else None
+    #    self.grad = np.zeros_like(self.data)
        self._backward = lambda: None
 
     def __repr__(self):
         # return f"Tensor(data={self.data}, dtype={type(self.data)})"
-        return f"Tensor(data={self.data}, grad={self.grad}, children={self._prev})"
+        return f"Tensor(data={self.data}, grad={self.grad})"
+
+    def set_requires_grad(self):
+        '''
+        Manages the required_grad parameter for a given node.
+        If a node is a leaf node ie no children, then it does not require grad.
+        '''
+        if self._prev != set():
+            self.requires_grad = True
 
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data + other.data, (self, other), '+')
-
+        out.set_requires_grad()
+        if out.grad is None:
+            out.grad = np.zeros_like(out.data)
+        else:
+            out.grad = out.grad
+        
         def _backward():
-            # if self.requires_grad:
+            if self.requires_grad:
                 self.grad += out.grad
-            # if other.requires_grad:
+            if other.requires_grad:
                 other.grad += out.grad
         out._backward = _backward
         return out
@@ -34,16 +47,29 @@ class Tensor:
     def __radd__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(other.data + self.data, (other, self), '+')
+        out.set_requires_grad()
+        if out.grad is None:
+            out.grad = np.zeros_like(out.data)
+        else:
+            out.grad = out.grad
 
         def _backward():
-            # if self.requires_grad:
+            if self.requires_grad:
                 self.grad += out.grad
-            # if other.requires_grad:
+            if other.requires_grad:
                 other.grad += out.grad
         out._backward = _backward
 
         return out
     
+    def set_requires_grad(self):
+        '''
+        Manages the required_grad parameter for a given node.
+        If a node is a leaf node ie no children, then it does not require grad.
+        '''
+        if self._prev != set():
+            self.requires_grad = True
+
 
     def backward(self):
         topo = []
@@ -58,7 +84,8 @@ class Tensor:
 
         # go one variable at a time and apply the chain rule to get its gradient
         self.grad = np.ones_like(self.data)
-        print(reversed(topo))
+        # print(topo)
         for v in reversed(topo):
+            print(v)
             v._backward()
         
--- src/trial.py
+++ src/trial.py
@@ -1,15 +1,68 @@
 from engine import Tensor
 import numpy as np
+from graphviz import Digraph
 
-tensor_a = Tensor([1,2])
-tensor_b = [3,1]
-out = tensor_a + tensor_b
-out.backward()
-# print("Addition:", out)
-# out.grad = np.ones_like(out.data)
-# out._backward()
-# print(out.grad)
-# print("Gradients of children")
-# print(tensor_a.grad)
-# print(tensor_b.grad)
-print(out)
+def tensor_to_string(tensor):
+    """Converts tensor data to a string representation."""
+    if tensor is not None:
+        if tensor.ndim > 1 or tensor.size > 4:  # Adjust based on preference
+            return f"shape {tensor.shape}"
+        else:
+            return ', '.join(f"{x:.4f}" for x in tensor.flatten())
+    else:
+        return "None"
+
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root, format='svg', rankdir='LR'):
+    """
+    format: png | svg | ...
+    rankdir: TB (top to bottom graph) | LR (left to right)
+    """
+    assert rankdir in ['LR', 'TB']
+    nodes, edges = trace(root)
+    dot = Digraph(format=format, graph_attr={'rankdir': rankdir})
+    
+    for n in nodes:
+        data_str = tensor_to_string(n.data)
+        grad_str = tensor_to_string(n.grad)
+        label = f"{{ data {data_str} | grad {grad_str} }}"
+        dot.node(name=str(id(n)), label=label, shape='record')
+        if hasattr(n, '_opt') and n._opt:
+            opt_node_name = str(id(n)) + n._opt
+            dot.node(name=opt_node_name, label=n._opt)
+            dot.edge(opt_node_name, str(id(n)))
+    
+    for n1, n2 in edges:
+        edge_start = str(id(n1))
+        edge_end = str(id(n2))
+        if hasattr(n2, '_opt') and n2._opt:
+            edge_end += n2._opt
+        dot.edge(edge_start, edge_end)
+    dot.render('graph/diagram')
+    return dot
+
+# Example usage
+tensor_a = Tensor(np.array([1,2]))
+tensor_b = Tensor(np.array([3,1]))
+tensor_c = Tensor(np.array([5,6]))
+tensor_d = (([9,8]))
+out1 = tensor_a + tensor_b
+# print(out1.grad)
+out2 = out1+out1+out1+out1
+# print("This is midmid gradient which is 2*mid", out2.grad)
+out3 = tensor_c + tensor_d
+out = out3 + out2
+out.backward()  
+graph = draw_dot(out)
+# print(tensor_d)
+
