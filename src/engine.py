Intermediate commit, need to fix broadcasting for backprop

src/engine.py
@@ -6,13 +6,14 @@ class Tensor:
     '''
     A class that stores tensor objects and their gradients
     '''
-    def __init__(self, data: Array, _children = (), _opt = ''):
+    def __init__(self, data, _children = (), _opt = '', requires_grad = False):
        self.data = data if isinstance(data, np.ndarray) else np.array(data)
+       if self.data.ndim == 0:  # If it's a scalar, reshape it to (1,)
+            self.data = self.data.reshape(1)
        self._opt = _opt
        self._prev = set(_children)
-       self.requires_grad = False
+       self.requires_grad = requires_grad
        self.grad = np.zeros_like(self.data) if self.requires_grad else None
-    #    self.grad = np.zeros_like(self.data)
        self._backward = lambda: None
 
     def __repr__(self):
@@ -33,8 +34,6 @@ class Tensor:
         out.set_requires_grad()
         if out.grad is None:
             out.grad = np.zeros_like(out.data)
-        else:
-            out.grad = out.grad
         
         def _backward():
             if self.requires_grad:
@@ -46,12 +45,10 @@ class Tensor:
     
     def __radd__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
-        out = Tensor(other.data + self.data, (other, self), '+')
+        out = Tensor(self.data + other.data, (other, self), '+')
         out.set_requires_grad()
         if out.grad is None:
             out.grad = np.zeros_like(out.data)
-        else:
-            out.grad = out.grad
 
         def _backward():
             if self.requires_grad:
@@ -62,15 +59,21 @@ class Tensor:
 
         return out
     
-    def set_requires_grad(self):
-        '''
-        Manages the required_grad parameter for a given node.
-        If a node is a leaf node ie no children, then it does not require grad.
-        '''
-        if self._prev != set():
-            self.requires_grad = True
+    def __mul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data*other.data, (other, self), '*')
+        out.set_requires_grad()
+        if out.grad is None:
+            out.grad = np.zeros_like(out.data)
 
-
+        def _backward():
+            if self.requires_grad:
+                self.grad += out.grad*other.data 
+            if other.requires_grad:
+                other.grad += out.grad*self.data 
+        out._backward = _backward
+        return out
+    
     def backward(self):
         topo = []
         visited = set()
@@ -83,9 +86,9 @@ class Tensor:
         build_topo(self)
 
         # go one variable at a time and apply the chain rule to get its gradient
+        # if self.grad == np.zeros_like(self.data):
         self.grad = np.ones_like(self.data)
-        # print(topo)
         for v in reversed(topo):
             print(v)
             v._backward()
-        
+

src/example.py
@@ -1,6 +1,7 @@
 from engine import Tensor
 import numpy as np
 from graphviz import Digraph
+import torch
 
 def tensor_to_string(tensor):
     """Converts tensor data to a string representation."""
@@ -52,16 +53,19 @@ def draw_dot(root, format='svg', rankdir='LR'):
     return dot
 
 # Example usage
-tensor_a = Tensor(np.array([1,2]))
-tensor_b = Tensor(np.array([3,1]))
-tensor_c = Tensor(np.array([5,6]))
-tensor_d = (([9,8]))
+tensor_a = Tensor(np.array([1,2,1]))
+tensor_b = Tensor(np.array([3,1,1]))
+tensor_c = Tensor(np.array([5,6,1]))
+tensor_d = (([9,8,1]))
 out1 = tensor_a + tensor_b
-# print(out1.grad)
-out2 = out1+out1+out1+out1
-# print("This is midmid gradient which is 2*mid", out2.grad)
+new = Tensor([[1, 2, 3], [4, 5, 6]])
+out2 = out1*new
+print("Out1", out1)
+print("Out2",out2)
 out3 = tensor_c + tensor_d
-out = out3 + out2
+outmid = out2+out2+out2
+out = out3 + outmid
+print("Out", out)
 out.backward()  
 graph = draw_dot(out)
 # print(tensor_d)